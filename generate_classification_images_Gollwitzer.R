6806bc7385a20aac34521d3c3e5872140d3e616b
Anton Gollwitzer <anton.gollwitzer@gmail.com>
2020-02-13 17:14:56 -0500

    Update generate_classification_images_Gollwitzer.R
    Added analysis code for individual participant generated faces/classification images.

--- generate_classification_images_Gollwitzer.R
+++ generate_classification_images_Gollwitzer.R
@@ -15,13 +15,23 @@ rcdata$StimR = as.numeric(rcdata$StimR)
 rcdata$response = as.numeric(rcdata$response)
 
 
+
 #NOTE: the 'rcic_seed_1_time_Sep_19_2019_16_34.Rdata' below needs to be replaced by the Rdata output file that was 
 #produced when you created the stimuli/face images earlier. 
 #the base name face needs to be replaced with the name of the base face that you used when you created
 #the stimuli/face images earlier. 
 
-# Generate CI
+# Generate Overall CI
 ci <- generateCI2IFC(rcdata$StimR, rcdata$response, 'mnes', 'rcic_seed_1_time_Sep_19_2019_16_34.Rdata', scaling='matched')
 
-# Generate anti-CI
+# Generate Overall anti-CI
 ci <- generateCI2IFC(rcdata$StimR, rcdata$response, 'mnes', 'rcic_seed_1_time_Sep_19_2019_16_34.Rdata', scaling='matched', antiCI=T)
+
+
+# Generate CI for each participant
+cis <- batchGenerateCI2IFC(rcdata, "ID", "StimR", "response", 'mnes', 'rcic_seed_1_time_Sep_19_2019_16_34.Rdata')
+
+# Generate CI for each condition
+# You must add a condition column in the results csv file and manually label each response according
+# to which condition someone was in. Make sure the header of this column is "Condition"  
+cis <- batchGenerateCI2IFC(rcdata, "Condition", "StimR", "response", 'mnes', 'rcic_seed_1_time_Sep_19_2019_16_34.Rdata')
